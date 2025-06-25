251277b1386e9802454ef79c10b58add95c01f61
Piyas <piyasreza31@gmail.com>
2025-06-25 18:56:27 +0600

    add header.jsx

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({subsets: ["latin"]})
 
@@ -12,8 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${inter.className}`}
-      >
+        className={`${inter.className}`}>
+          <Header/>
 
         <main className="min-h-screen">
            {children}
