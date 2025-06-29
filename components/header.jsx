ea353555c2ff7bd0a2ede8f5539e2996b667ca28
Piyas <piyasreza31@gmail.com>
2025-06-30 02:04:30 +0600

    store user in db

--- components/header.jsx
+++ components/header.jsx
@@ -4,10 +4,13 @@ import Link from 'next/link';
 import React from 'react';
 import { Button } from './ui/button';
 import { ArrowLeft, CarFront, Heart } from 'lucide-react';
+import { checkUser } from '@/lib/checkUser';
 
 
 const Header = async ({isAdminPage=false}) => {
-const isAdmin = false
+const user = await checkUser();
+
+const isAdmin = user?.role === "ADMIN";
 
   return (
     <header className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
