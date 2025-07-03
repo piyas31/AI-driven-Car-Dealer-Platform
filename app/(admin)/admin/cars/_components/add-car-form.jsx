c0bd4eb5d7daececeeb9a1a4bd1642647819a1b4
Piyas <piyasreza31@gmail.com>
2025-07-03 18:03:29 +0600

    starting add car form ui

--- app/(admin)/admin/cars/_components/add-car-form.jsx
+++ app/(admin)/admin/cars/_components/add-car-form.jsx
@@ -1,6 +1,39 @@
 import React from 'react'
 
+const fuelTypes = ["Petrol", "Diesel", "Electric", "Hybrid", "Plug-in Hybrid"];
+const transmissions = ["Automatic", "Manual", "Semi-Automatic"];
+const bodyTypes = [
+  "SUV",
+  "Sedan",
+  "Hatchback",
+  "Convertible",
+  "Coupe",
+  "Wagon",
+  "Pickup",
+];
+const carStatuses = ["AVAILABLE", "UNAVAILABLE", "SOLD"];
+
 const AddCarForm = () => {
+
+  const carFormSchema = z.object({
+    make: z.string().min(1,"Make is required"),
+    model: z.string().min(1,"Model is required"),
+    year: z.string().refine((val) => {
+    const year = parseInt(val);
+    return !isNaN(year) && year >= 1900 && year <= new Date().getFullYear() + 1;
+  }, "Valid year required"),
+  price: z.string().min(1, "Price is required"),
+  mileage: z.string().min(1, "Mileage is required"),
+  color: z.string().min(1, "Color is required"),
+  fuelType: z.string().min(1, "Fuel type is required"),
+  transmission: z.string().min(1, "Transmission is required"),
+  bodyType: z.string().min(1, "Body type is required"),
+  seats: z.string().optional(),
+  description: z.string().min(10, "Description must be at least 10 characters"),
+  status: z.enum(["AVAILABLE", "UNAVAILABLE", "SOLD"]),
+  featured: z.boolean().default(false),
+  })
+
   return (
     <div>AddCarForm</div>
   )
