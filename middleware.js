5e111e2159773778879ec1941a7d07485d42b183
Piyas <piyasreza31@gmail.com>
2025-07-15 00:07:46 +0600

    fixed some error

--- middleware.js
+++ middleware.js
@@ -3,19 +3,25 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
+  "/admin(.*)",
   "/saved-cars(.*)",
   "/reservations(.*)",
 ]);
 
+
 const aj = arcjet({
   key: process.env.ARCJET_KEY,
+ 
   rules: [
+   
     shield({
       mode: "LIVE",
     }),
     detectBot({
-      mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE"],
+      mode: "LIVE", 
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", 
+      ],
     }),
   ],
 });
@@ -36,8 +42,9 @@ export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
-    "/saved-cars(.*)",
-    "/reservations(.*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
