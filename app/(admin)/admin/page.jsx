170ae05d34dd2b2645b861dec17831f98f9a96f0
Piyas <piyasreza31@gmail.com>
2025-07-14 23:44:46 +0600

    fixed bug for deployment

--- app/(admin)/admin/page.jsx
+++ app/(admin)/admin/page.jsx
@@ -2,6 +2,8 @@ import { getDashboardData } from "@/actions/admin";
 import { Dashboard } from "./_components/dashboard";
 
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 
 
 export const metadata = {
